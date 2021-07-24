c61f728d128aa7c3b624e29fb5d640a98c6cdf34
arturtondera <87845977+arturtondera@users.noreply.github.com>
2021-07-24 06:49:31 +0200

    add dict new weapon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Hiszpańska inkwizycja to najlepszy skecz Monty Pythona")
 inquisition = {
-  "main weapon": "fear"
+  "main weapon": "fear", "surprise"
 }
 for i in inquisition:
   print("Our main weapon is:", inquisition.values())
